6acfb23d0ef9581d5e3838260dc09083e59d9856
hellogaon <la_vega_@naver.com>
2025-11-12 00:39:40 +0900

    [sql] reset sql queries

--- sql/pq2_explain.sql
+++
@@ -1,9 +0,0 @@
-EXPLAIN SELECT count(*)
-FROM Person_knows_Person
-JOIN Comment_hasCreator_Person
-  ON Person_knows_Person.Person1Id = Comment_hasCreator_Person.PersonId
-JOIN Comment_replyOf_Post
-  ON Comment_hasCreator_Person.CommentId = Comment_replyOf_Post.CommentId
-JOIN Post_hasCreator_Person
-  ON Person_knows_Person.Person2Id = Post_hasCreator_Person.PersonId 
-  AND Post_hasCreator_Person.PostId = Comment_replyOf_Post.PostId
--- sql/pq7.sql
+++
@@ -1,8 +0,0 @@
-SELECT count(*)
-FROM Message_hasTag_Tag
-JOIN Message_hasCreator_Person
-  ON Message_hasTag_Tag.MessageId = Message_hasCreator_Person.MessageId
-LEFT JOIN Comment_replyOf_Message 
-  ON Comment_replyOf_Message.MessageId = Message_hasTag_Tag.MessageId
-LEFT JOIN Person_likes_Message
-  ON Person_likes_Message.MessageId = Message_hasTag_Tag.MessageId;
