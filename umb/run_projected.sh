fd2728290a7af1e63d9311ba5e8ee978ed20d0f1
hellogaon <la_vega_@naver.com>
2025-11-12 00:29:40 +0900

    [umb] Add projected and explain benchmark scripts

---
+++ umb/run_projected.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+. umb/vars.sh
+. scripts/import-vars.sh
+
+python3 pos/client_projected.py ${SF} Umbra $@
