68f8a40e1e254268f7fe98f290c2ec796697b625
hellogaon <la_vega_@naver.com>
2025-11-12 00:21:28 +0900

    [kuzu] Add benchmark execution scripts

---
+++ kuz/run_benchmark.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+
+. kuz/vars.sh
+. scripts/import-vars.sh
+
+QUERY_IDS=("1" "1-short" "2" "3" "4" "5" "6" "7" "8" "9")
+REPEAT_TIMES=1
+TIMEOUT_DURATION="1h"
+
+for QUERY_ID in "${QUERY_IDS[@]}"; do
+  for ((i=1; i<=REPEAT_TIMES; i++)); do
+    sync
+    sudo sh -c "echo 1 > /proc/sys/vm/drop_caches"
+    sudo sh -c "echo 2 > /proc/sys/vm/drop_caches"
+    sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+    
+    timeout "${TIMEOUT_DURATION}" python3 kuz/client.py ${SF} ${QUERY_ID} $@ || {
+      echo "QUERY_ID ${QUERY_ID} timed out after ${TIMEOUT_DURATION}. Moving to next query."
+    }
+  done
+done
